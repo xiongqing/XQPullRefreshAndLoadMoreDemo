6b1466049addf93d332075d0b8f5025729126095
xiongqing <1256496127@qq.com>
2014-02-24 16:10:09 +0800

    Initial Commit

---
+++ XQPullRefreshAndLoadMoreDemo/XQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XQAppDelegate.h
+//  XQPullRefreshAndLoadMoreDemo
+//
+//  Created by HySoft on 14-2-24.
+//  Copyright (c) 2014年 HySoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XQPullRefreshAndLoadMoreDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XQPullRefreshAndLoadMoreDemo
+//
+//  Created by HySoft on 14-2-24.
+//  Copyright (c) 2014年 HySoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XQAppDelegate class]));
+    }
+}
